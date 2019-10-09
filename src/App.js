24e18445091a97b359be045c7f45fc1c562f2944
Brian <briandsalemi@gmail.com>
2019-10-09 16:14:10 -0400

    added a bit of html and css to App.js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="app-header">
+        - Will be header component -
+      <img src="https://fontmeme.com/permalink/190910/cd6f2ab7e2bb65d7267bc46d5894d410.png" alt="plants-letters-font" border="0"/>
+      <p class="slogan">
+                <img src="https://fontmeme.com/permalink/190910/e82e091f0a56a273adfbca896400ea3b.png" alt="miralight-font" border="0"/>
+      </p>
+      SEARCH BAR?
+      </div>
+      <div id="plant-container">
+        plants
+      </div>
+
     </div>
   );
 }
