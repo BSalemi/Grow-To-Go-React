81a253acb33a8c91f7913707af05a1673aa995f4
Brian <briandsalemi@gmail.com>
2019-10-09 18:22:25 -0400

    added onChange event and  handleOnChange function to LoginForm Component to update state.name and state.email with inputs

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,13 +6,19 @@ export default class LoginForm extends React.Component {
         name: "",
         email: ""
     }
+
+    handleOnChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
     render(){
         return(
             <div id="login form">
                 <form>
-                    <input type="text" name="name" placeholder="Please enter your name" value={this.state.name}/> 
+                    <input type="text" name="name" placeholder="Please enter your name" value={this.state.name} onChange={event => this.handleOnChange(event)}/> 
                     <br/>
-                    <input type="text" name="email" placeholder="Please enter your email" value={this.state.email}/>
+                    <input type="text" name="email" placeholder="Please enter your email" value={this.state.email} onChange={event => this.handleOnChange(event)}/>
                     <br/>
                     <input type="submit" value="Get Shopping!"/>
                 </form>
