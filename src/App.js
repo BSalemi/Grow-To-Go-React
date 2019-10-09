ae52abc18490715bfbfe154878996b443a1886df
Brian <briandsalemi@gmail.com>
2019-10-09 16:21:22 -0400

    created components folder and Header component file. Moved header div into Header component and added Header component to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <div id="app-header">
-        - Will be header component -
-      <img src="https://fontmeme.com/permalink/190910/cd6f2ab7e2bb65d7267bc46d5894d410.png" alt="plants-letters-font" border="0"/>
-      <p class="slogan">
-                <img src="https://fontmeme.com/permalink/190910/e82e091f0a56a273adfbca896400ea3b.png" alt="miralight-font" border="0"/>
-      </p>
+      <Header/>
       SEARCH BAR?
-      </div>
+    
       <div id="plant-container">
         plants
       </div>
