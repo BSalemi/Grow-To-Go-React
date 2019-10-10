f957c457ab53e963a4d2eb8fd4aa70334c939c9f
Brian <briandsalemi@gmail.com>
2019-10-10 18:15:12 -0400

    created SearchBar component and input form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header'
 import LoginForm from './components/LoginForm'
 import PlantContainer from './containers/PlantContainer';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <div>
           <Header/>
           <Route exact path="/login" component={LoginForm} />
-          
+          <SearchBar/>
           <PlantContainer/>
         </div>
       </Router>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,24 @@
 import React from 'react'
 
-export default class SearchBar extends Component {
-    
+export default class SearchBar extends React.Component {
+    state = {
+        value: ""
+    }
+
+    handleOnChange = (event) => {
+        this.setState({
+            value: event.target.value
+        })
+    }
+ 
+    render(){
+        return(
+            <div className="searchForm">
+                <form>
+                    <input type="text" value={this.state.value} onChange={event => this.handleOnChange(event)}/>
+                    <input type="submit" value="Search"/>
+                </form>
+            </div>
+        )
+    }
 }
