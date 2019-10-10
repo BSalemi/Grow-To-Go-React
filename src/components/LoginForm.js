16a5892324f4c46451b8e862a81fe17cb03a5d1d
Brian <briandsalemi@gmail.com>
2019-10-09 20:05:44 -0400

    created onSubmit function for login form, which sends a post fetch request to create a user. added basic boilerplate to set up the store in index.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+const USERS_URL = "http://localhost:3000/users"
+
+
 export default class LoginForm extends React.Component {
 
     state = {
@@ -7,15 +10,34 @@ export default class LoginForm extends React.Component {
         email: ""
     }
 
-    handleOnChange = (event) => {
+    handleOnChange = event => {
         this.setState({
             [event.target.name]: event.target.value
         })
     }
+
+    handleOnSubmit = event => {
+        event.preventDefault()
+        const user = {
+            name: this.state.name,
+            email: this.state.email
+        }
+        fetch(USERS_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                 Accept: "application/json"
+        },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(user_data => console.log(user_data, "user_data"))
+    }
     render(){
         return(
             <div id="login form">
-                <form>
+                {/* <h4>Welcome to GrowToGo. Enter your name and email to get started. </h4> */}
+                <form onSubmit={event => this.handleOnSubmit(event)}>
                     <input type="text" name="name" placeholder="Please enter your name" value={this.state.name} onChange={event => this.handleOnChange(event)}/> 
                     <br/>
                     <input type="text" name="email" placeholder="Please enter your email" value={this.state.email} onChange={event => this.handleOnChange(event)}/>
--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// import { Provider } from 'react-redux'
-// import { createStore, applyMiddleware } from 'redux'
-// import thunk from 'redux-thunk'
-// import reducer from '/reducer'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
-// const store = createStore()
+
+const store = createStore(()=> [], {}, applyMiddleware(thunk))
 
 ReactDOM.render(
-
-    <App/>
-
+    <Provider store={store}>
+        <App/>
+    </Provider>
 , 
 document.getElementById('root')
 );
