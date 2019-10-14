722c90cdf0e5a793472876e957427383226b7e00
Brian <briandsalemi@gmail.com>
2019-10-14 11:32:37 -0400

    created a users-cart div and moved the onClick event from cart-box to there. that way the future onClick event for checkout button is not closing the cart instead

--- src/containers/CartPlantContainer.js
+++ src/containers/CartPlantContainer.js
@@ -50,10 +50,12 @@ class CartPlantContainer extends Component {
                <button id='cart-btn' onClick={this.handleOnClick}><img src="https://img.icons8.com/ios-filled/50/000000/shopping-cart-loaded.png"/></button>
                
               
-              <div id="cart-box" onClick={this.handleOnClick}>
-                {this.renderCart()}
-                {this.props.user.carts && this.generateCartPlants()}
-                Total - ${this.props.user.carts && this.props.user.carts[this.props.user.carts.length - 1].total}
+              <div id="cart-box" >
+                <div id="users-cart" onClick={this.handleOnClick}>
+                    {this.renderCart()}
+                    {this.props.user.carts && this.generateCartPlants()}
+                    Total - ${this.props.user.carts && this.props.user.carts[this.props.user.carts.length - 1].total}
+                </div>
                 <button id="checkout">Checkout</button>
               </div>
             </div>
