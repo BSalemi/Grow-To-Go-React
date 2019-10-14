2081a73570c16d5113416e1dbb6e4b9a7ed7c5be
Brian <briandsalemi@gmail.com>
2019-10-14 12:57:47 -0400

    added add and remove icons to the cart

--- src/components/CartPlant.js
+++ src/components/CartPlant.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import {addIcon, removeIcon} from '../constants.js'
 export default class CartPlant extends Component {
 
 
@@ -7,7 +7,7 @@ export default class CartPlant extends Component {
     render() {
         return (
             <div id="users-cart-plants">
-                <strong>{this.props.name}</strong> - ${this.props.price}
+                <button id="add-cart-plant"><img src={`${addIcon}`}/></button><button id="remove-cart-plant"><img src={`${removeIcon}`}/></button><strong>{this.props.name}</strong> - ${this.props.price}
             </div>
         )
     }
