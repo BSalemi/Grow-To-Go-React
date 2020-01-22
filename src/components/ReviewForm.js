53f5e8478a9f8883134d654d86001df59b229afc
Brian <briandsalemi@gmail.com>
2020-01-22 18:58:32 -0500

    added state to ReviewForm component and added value to title and body input areas

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,12 +1,17 @@
 import React from 'react'; 
 
 export default class ReviewForm extends React.Component {
+    state = {
+        title: "",
+        body: ""
+    }
+
     render() {
         return(
             <div className="review-form">
                 <form>
-                    <input type="text" name="title" placeholder="Review Title"/>
-                    <input type="textarea" name="body" placeholder="Review Body"/>
+                    <input type="text" name="title" placeholder="Review Title" value={this.state.title}/>
+                    <input type="textarea" name="body" placeholder="Review Body" value={this.state.body}/>
                     <input type="submit"/>
                 </form>
             </div>
