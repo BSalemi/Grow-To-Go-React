2f0c791bc9bf9636a93c1667c79b8af8a81db253
Brian <briandsalemi@gmail.com>
2020-01-22 20:31:48 -0500

    created handleOnChange function and added event listener to the text and textarea inputs to set ReviewForm state onChange

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -6,12 +6,18 @@ export default class ReviewForm extends React.Component {
         body: ""
     }
 
+    handleOnChange = (event)=>{
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render() {
         return(
             <div className="review-form">
                 <form>
-                    <input type="text" name="title" placeholder="Review Title" value={this.state.title}/>
-                    <input type="textarea" name="body" placeholder="Review Body" value={this.state.body}/>
+                    <input type="text" name="title" placeholder="Review Title" value={this.state.title} onChange={this.handleOnChange}/>
+                    <input type="textarea" name="body" placeholder="Review Body" value={this.state.body} onChange={this.handleOnChange}/>
                     <input type="submit"/>
                 </form>
             </div>
