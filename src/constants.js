a01e3cbac88ce7a43828839df9b5bb1ccd7cbb7d
Brian <briandsalemi@gmail.com>
2020-02-08 08:57:04 -0500

    optimized icon photos for better performance results

--- src/constants.js
+++ src/constants.js
@@ -5,8 +5,9 @@ export const CART_PLANTS_URL = `${BASE_URL}/cart_plants`
 export const CHECKOUT_URL = `${BASE_URL}/checkout`
 export const REVIEWS_URL = `${BASE_URL}/reviews`
 
-export const addIcon = "https://cdn2.iconfinder.com/data/icons/bwpx/icons/symbol_addition.gif"
-export const removeIcon = "http://icons.iconarchive.com/icons/icons8/windows-8/16/Editing-Delete-icon.png"
-export const cartIcon = "https://img.icons8.com/ios-filled/50/000000/shopping-cart-loaded.png"
-export const logoutIcon = "http://icons.iconarchive.com/icons/icons8/windows-8/32/User-Interface-Logout-icon.png"
-export const deleteReview = "https://cdn0.iconfinder.com/data/icons/octicons/1024/x-16.png"
+export const addIcon = "https://www.dropbox.com/s/2e6bvgtgvw3yb8x/add_cart_symbol.gif?raw=1"
+export const removeIcon = "https://www.dropbox.com/s/7x5ojym9p2cfsf5/delete_cart_icon.png?raw=1"
+export const cartIcon = "https://www.dropbox.com/s/eh9ccrgnf2eh39z/cart_icon.png?raw=1"
+export const logoutIcon = "https://www.dropbox.com/s/ud4tcq345l6g8gg/logout_icon.png?raw=1"
+export const deleteReview = "https://www.dropbox.com/s/njxzlu213m8nvne/delete_review_icon.png?raw=1"
+
