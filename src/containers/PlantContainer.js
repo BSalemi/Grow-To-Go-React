99f73ef96605253241e7cda44706929b7113d518
Brian <briandsalemi@gmail.com>
2020-03-06 12:04:13 -0500

    began changing filterVisiblePlants() to include multiple if statements, not switch statement, so that multiple filters can be applied at the same time

--- src/containers/PlantContainer.js
+++ src/containers/PlantContainer.js
@@ -23,49 +23,66 @@ class PlantContainer extends Component {
         
     filterVisiblePlants = () => { 
         const filtered = this.filterPetFriendly()
-        console.log(this.props.visibilityFilter, "visibilityFilter")
-        for(let filter in this.props.visibilityFilter){
-            
-            console.log(filter, "filter in filterVisiblePlants")
-        switch(filter){
-            case "sort_price_asc":
-                return filtered.sort((a, b) => (a.price > b.price) ? 1 : -1 );   
-            case "sort_price_desc":
-                return filtered.sort((a, b) => (a.price < b.price) ? 1 : -1 );
-            case "find_mini":
-                return filtered.filter(plant => plant.size === "Mini");
-            case "find_small":
-                return filtered.filter(plant => plant.size === "Small");
-            case "find_medium":
-                return filtered.filter(plant => plant.size === "Medium");
-            case "find_large":
-                return filtered.filter(plant => plant.size === "Large");
-            case "find_xlarge":
-                return filtered.filter(plant => plant.size === "X-Large");
-            case "find_beginner":
-                return filtered.filter(plant => plant.exp_level === "Beginner");
-            case "find_intermediate":
-                return filtered.filter(plant => plant.exp_level === "Intermediate");
-            case "find_advanced":
-                return filtered.filter(plant => plant.exp_level === "Advanced");
-            case "find_low":
-                return filtered.filter(plant => plant.light_required.includes("Low"));
-            case "find_medium_indirect":
-                return filtered.filter(plant => plant.light_required.includes("Medium"));
-            case "find_bright_indirect":
-                return filtered.filter(plant => plant.light_required.includes("Bright Indirect"));
-            case "find_bright_direct":
-                return filtered.filter(plant => plant.light_required.includes("Bright Direct"));
-            case "find_full_sun":
-                return filtered.filter(plant => plant.light_required.includes("Full Sun"));  
-            default:
-                return filtered    
-        }}   
+        console.log(filtered, "filterPetFriendly")
+        
+        const visFilter = this.props.visibilityFilter
+        
+        if(visFilter.price === "sort_price_asc"){
+            return filtered.sort((a, b) => (a.price > b.price) ? 1 : -1 ); 
+        }
+        if(visFilter.price === "sort_price_desc"){
+            return filtered.sort((a, b) => (a.price < b.price) ? 1 : -1 );
+        }
+        if(visFilter.size === "find_mini"){
+            return filtered.filter(plant => plant.size === "Mini");
+        }
+        if(visFilter.size === "find_small"){
+            return filtered.filter(plant => plant.size === "Small")
+        }
+        if(visFilter.size === "find_medium"){
+            return filtered.filter(plant => plant.size === "Medium")
+        }
+        if(visFilter.size === "find_large"){
+            return filtered.filter
+        }
+        if(visFilter.size === "find_xlarge"){
+            return filtered.filter(plant => plant.size === "X-Large")
+        }
+        return filtered
+                    // case "find_mini":
+                        
+                    // case "find_small":
+                    //     return filtered.filter(plant => plant.size === "Small");
+                    // case "find_medium":
+                    //     return filtered.filter(plant => plant.size === "Medium");
+                    // case "find_large":
+                    //     return filtered.filter(plant => plant.size === "Large");
+                    // case "find_xlarge":
+                    //     return filtered.filter(plant => plant.size === "X-Large");
+                    // case "find_beginner":
+                    //     return filtered.filter(plant => plant.exp_level === "Beginner");
+                    // case "find_intermediate":
+                    //     return filtered.filter(plant => plant.exp_level === "Intermediate");
+                    // case "find_advanced":
+                    //     return filtered.filter(plant => plant.exp_level === "Advanced");
+                    // case "find_low":
+                    //     return filtered.filter(plant => plant.light_required.includes("Low"));
+                    // case "find_medium_indirect":
+                    //     return filtered.filter(plant => plant.light_required.includes("Medium"));
+                    // case "find_bright_indirect":
+                    //     return filtered.filter(plant => plant.light_required.includes("Bright Indirect"));
+                    // case "find_bright_direct":
+                    //     return filtered.filter(plant => plant.light_required.includes("Bright Direct"));
+                    // case "find_full_sun":
+                    //     return filtered.filter(plant => plant.light_required.includes("Full Sun"));  
+                    // default:
+                        
+
+        
     }
 
     generatePlants = () => {
-        let values = this.props.visibilityFilter.values 
-        if((values || this.props.petFriendly) && this.props.search === ""){
+        if(this.filterVisiblePlants() && (this.props.visibilityFilter || this.props.petFriendly) && this.props.search === ""){
             return this.filterVisiblePlants().map(plant => {
             return <div className="plant-card" key={plant.id}>
             <Plant id={plant.id} image={plant.image} size={plant.size} price={plant.price} name={plant.name} species={plant.species} exp_level={plant.exp_level} light_required={plant.light_required} pet_friendly={plant.pet_friendly}/>
