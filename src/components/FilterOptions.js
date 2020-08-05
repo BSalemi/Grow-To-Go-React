d2181646854bc04b475e1ec57e2c619548fc5b32
Brian <briandsalemi@gmail.com>
2020-08-05 18:43:06 -0400

    Changed FilterOptions component into a class component. Created a .toggle-filter div with an event handler called toggleFilters. When the div is clicked toggleFilters applies classes to the .sort-menus and .toggle-filter elements that change their position and visibility. Added the CSS styling for these new classes to index.css

--- src/components/FilterOptions.js
+++ src/components/FilterOptions.js
@@ -1,53 +1,71 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 
-const FilterOptions = (props) => {
+class FilterOptions extends Component {
+
+    toggleFilters = () => {
+        const menus = document.querySelector(".sort-menus")
+        const filterToggle = document.querySelector(".toggle-filter")
+
+        menus.classList.toggle('hidden')
+        filterToggle.classList.toggle('toggle-clicked')
+    }
+
+    render(){
+        const {setVisibilityFilter, setPetFriendly} = this.props 
+
         return (
+        <>
+            <div className="toggle-filter" onClick={this.toggleFilters}>Filter Options</div>
 
-            <div className="sort-menus">
-            <div className="sort-menu-children">
-                <label value="select-price"><strong style={{padding: "3px"}}>Price:</strong></label>
-                <select onChange={props.setVisibilityFilter} id="price-dropdown" name="price">
-                    <option value="sort_price_asc">Low to High</option>
-                    <option value="sort_price_desc">High to Low</option>
-                </select>
-            </div>
-            <div className="sort-menu-children">
-                <label value="select-size"><strong style={{padding: "3px"}}>Size:</strong></label>
-                <select onChange={props.setVisibilityFilter} id="size-dropdown" name="size">
-                    <option  value="find_mini">Mini</option>
-                    <option  value="find_small">Small</option>
-                    <option  value="find_medium">Medium</option>
-                    <option  value="find_large">Large</option>
-                    <option  value="find_xlarge">X-Large</option>
-                </select>
-            </div>
+            <div className="sort-menus hidden">
+                <div className="sort-menu-children">
+                    <label value="select-price"><strong style={{padding: "3px"}}>Price:</strong></label>
+                    <select onChange={setVisibilityFilter} id="price-dropdown" name="price">
+                        <option value="sort_price_asc">Low to High</option>
+                        <option value="sort_price_desc">High to Low</option>
+                    </select>
+                </div>
 
-            <div className="sort-menu-children">
-                <label value="select-exp-level"><strong style={{padding: "3px"}}>Experience:</strong></label>
-                <select onChange={props.setVisibilityFilter} id="exp-level-dropdown" name="expLevel">
-                    <option  value="find_beginner">Beginner</option>
-                    <option  value="find_intermediate">Intermediate</option>
-                    <option  value="find_advanced">Advanced</option>
-                </select>
-            </div>
+                <div className="sort-menu-children">
+                    <label value="select-size"><strong style={{padding: "3px"}}>Size:</strong></label>
+                    <select onChange={setVisibilityFilter} id="size-dropdown" name="size">
+                        <option  value="find_mini">Mini</option>
+                        <option  value="find_small">Small</option>
+                        <option  value="find_medium">Medium</option>
+                        <option  value="find_large">Large</option>
+                        <option  value="find_xlarge">X-Large</option>
+                    </select>
+                </div>
 
-            <div className="sort-menu-children">
-                <label value="select-light-required"><strong style={{padding: "3px"}}>Light Requirement:</strong></label>
-                <select onChange={props.setVisibilityFilter} id="light-required-dropdown" name="lightRequired">
-                    <option  value="find_low">Low Light</option>
-                    <option  value="find_medium_indirect">Medium Indirect</option>
-                    <option  value="find_bright_indirect">Bright Indirect</option>
-                    <option  value="find_bright_direct">Bright Direct</option>
-                    <option  value="find_full_sun">Full Sun</option>
-                </select>
+                <div className="sort-menu-children">
+                    <label value="select-exp-level"><strong style={{padding: "3px"}}>Experience:</strong></label>
+                    <select onChange={setVisibilityFilter} id="exp-level-dropdown" name="expLevel">
+                        <option  value="find_beginner">Beginner</option>
+                        <option  value="find_intermediate">Intermediate</option>
+                        <option  value="find_advanced">Advanced</option>
+                    </select>
+                </div>
+
+                <div className="sort-menu-children">
+                    <label value="select-light-required"><strong style={{padding: "3px"}}>Light Requirement:</strong></label>
+                    <select onChange={setVisibilityFilter} id="light-required-dropdown" name="lightRequired">
+                        <option  value="find_low">Low Light</option>
+                        <option  value="find_medium_indirect">Medium Indirect</option>
+                        <option  value="find_bright_indirect">Bright Indirect</option>
+                        <option  value="find_bright_direct">Bright Direct</option>
+                        <option  value="find_full_sun">Full Sun</option>
+                    </select>
+                </div>
+
+                <div className="sort-menu-children">
+                    <label value="select-pet-friendly"><strong style={{padding: "3px"}}>Pet Friendly: </strong></label>
+                    <input onClick={setPetFriendly}  defaultChecked={false} type="checkbox" id="pet-friendly-checkbox" value="pet-friendly"/>
+                </div>
             </div>
-            <div className="sort-menu-children">
-                <label value="select-pet-friendly"><strong style={{padding: "3px"}}>Pet Friendly: </strong></label>
-                <input onClick={props.setPetFriendly}  defaultChecked={false} type="checkbox" id="pet-friendly-checkbox" value="pet-friendly"/>
-            </div>
-        </div>
+        </>
         )
     }
+}
 
     export default FilterOptions
